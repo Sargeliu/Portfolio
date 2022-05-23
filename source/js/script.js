b74a44e4c2898ef5447b2908ff70ce7559c0f978
Sargeliu <sargeliu@mail.ru>
2022-05-23 20:01:24 +0500

    Форма обратной связи

--- source/js/script.js
+++ source/js/script.js
@@ -1,50 +1,66 @@
-let hamburger = document.querySelector('.hamburger__toggle');
-let menu = document.querySelector('.main-nav');
-let closeElem = document.querySelector('.main-nav__toggle');
-let about = document.querySelector('.site-list__link--about');
-let resume = document.querySelector('.site-list__link--resume');
-let skills = document.querySelector('.site-list__link--skills');
-let portfolio = document.querySelector('.site-list__link--portfolio');
-let price = document.querySelector('.site-list__link--price');
-let contacts = document.querySelector('.site-list__link--contacts');
+$(document).ready(function(){
 
+  // Modal
 
-hamburger.onclick = function() {
-  menu.classList.toggle('main-nav--closed');
-  menu.classList.toggle('main-nav--opened');
-}
+  $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
 
-closeElem.onclick = function() {
-  menu.classList.toggle('main-nav--closed');
-  menu.classList.toggle('main-nav--opened');
-}
+    $('.button__mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 
-about.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+    // Validates
 
-resume.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+  function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              message: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+              message: "Пожалуйста, введите сообщение",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+  }
 
-skills.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+  validateForms('form');
 
-portfolio.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-price.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+  $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('.overlay, #thanks').fadeIn('slow');
 
-contacts.onclick = function() {
-  menu.classList.remove('main-nav--opened');
-  menu.classList.add('main-nav--closed');
-}
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+});
